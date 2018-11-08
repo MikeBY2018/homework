060713b28fe54ce6779f194f82008312049a1d93
MikeBY2018 <babskij2015@gmail.com>
2018-11-09 00:43:52 +0300

    Правка 3=ей страницы и работа над пунктом меню Portfolio

--- endresult/js/main.js
+++ endresult/js/main.js
@@ -1,42 +1,44 @@
-window.onload = function(){
-    var slider = document.querySelectorAll('main .servises li');
-    for (var i = 0; i < slider.length; i++) {
-        slider[i].style.display = 'block'; 
+var slideNow = 1;
+var translateHeight = 0;
+var slideInterval = 3000;
+var slideCount = $('#slidewrapper').children().length;
+console.log(slideCount);
+
+function nextSlide() {
+    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+        $('#slidewrapper').css('transform', 'translate(0, 0)');
+        slideNow = 1;
+    } else {
+        translateHeight = -$('#viewport').height() * (slideNow);
+        $('#slidewrapper').css({
+            'transform': 'translate(0 ,' + translateHeight + 'px)',
+            '-webkit-transform': 'translate(0, ' + translateHeight + 'px)',
+            '-ms-transform': 'translate(0, ' + translateHeight + 'px)',
+        });
+        slideNow++;
     }
+};
+
+$(document).ready(function () {
+    setInterval(nextSlide, slideInterval);
+});    
+
+
+
+
+/*Portfolio*/
+var portfolio = document.getElementById('portfolio');
+var portfolioCo = document.getElementById('content-portfolio');
+var withoutPortfolio = document.querySelector('.cl-effect-4 li a');
+
+
+portfolio.onclick = function(){
+    portfolio.classList.add('this_page');
+    portfolioCo.style.display = 'block';
+    withoutPortfolio.classList.remove('this_page');
 }
-
-
-
-
-
-
-
-
-
-
-
-/*var hrefPage = document.querySelectorAll('.pages>a');
-var i = 0;
-document.onkeydown = function(e){
-    
-    if (e.keyCode == 38){
-        console.log(hrefPage[i].href);
-        window.location.href = hrefPage[i].href;
-        i--;
-    }
-    if (i < 0){
-        i = hrefPage.length - 1;
-    }    
-
-}    
-    
-    
-  if (e.keyCode == 40){  
-        // console.log(e);    
-        console.log(hrefPage[i].href);
-        window.location.href = hrefPage[i].href;
-    }
-    i--;*/
-
-
-    
+portfolioCo.onclick = function(){
+    portfolio.classList.remove('this_page');
+    portfolioCo.style.display = 'none';
+    withoutPortfolio.classList.add('this_page');
+}
