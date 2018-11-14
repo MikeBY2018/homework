c2d5c76a2f1274666a702a72133a691cd1e9d103
MikeBY2018 <babskij2015@gmail.com>
2018-11-14 16:01:31 +0300

    fixed slider from page_3 and fix page ContactUs

--- endresult/js/page3.js
+++ endresult/js/page3.js
@@ -5,24 +5,36 @@ var slideInterval = 3000;
 var slideCount = $('#slidewrapper').children().length;
 console.log(slideCount);
 
+
+var windowSlide = document.getElementById('windowSlider');
+var oneSlide =  document.querySelector('.uxui').offsetHeight + 6;
+
+
 function nextSlide() {
     if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
         $('#slidewrapper').css('transform', 'translate(0, 0)');
         slideNow = 1;
     } else {
-        translateHeight = -$('#viewport').height() * (slideNow);
+        translateHeight = -$('#viewport').height()/slideCount * (slideNow);
         $('#slidewrapper').css({
             'transform': 'translate(0 ,' + translateHeight + 'px)',
             '-webkit-transform': 'translate(0, ' + translateHeight + 'px)',
             '-ms-transform': 'translate(0, ' + translateHeight + 'px)',
         });
-        slideNow++;
-    }
+        slideNow++; 
+    };
 };
 
+windowSlide.style.height = oneSlide  + 'px';
+console.log(oneSlide);     
+
+
+/*Время, через которое запускается функция слайда*/
 $(document).ready(function () {
     setInterval(nextSlide, slideInterval);
 }); 
+
+
 
 
 /*Возврат выделения пункта меню, который указывает на то, на какой странице мы находимся*/
